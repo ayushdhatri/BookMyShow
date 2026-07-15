7368c5a7c9cf818927240763c9785ec696b36b94
Ayush Solanki <aayushdhatri@gmail.com>
2026-07-15 18:43:22 +0530

    Adding createdAt and LastModifiedDate

--- src/main/java/com/bookmyshow/models/BaseModel.java
+++ src/main/java/com/bookmyshow/models/BaseModel.java
@@ -1,20 +1,32 @@
 package com.bookmyshow.models;
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @MappedSuperclass
 @Getter
 @Setter
+@EntityListeners(EntityListeners.class)
 public class BaseModel {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
+
+
+
